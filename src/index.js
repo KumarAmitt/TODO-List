fe6768e9ef51cd1b64a9c4982ea0b075fe6e60ec
KumarAmitt <amitt2015@outlook.com>
2021-05-12 20:10:45 +0530

    Add input field for new Projects

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import './stylesheets/style.scss';
 
 const elements = {
   menu: document.querySelector('.menu'),
-  sidebar: document.querySelector('.sidebar')
+  sidebar: document.querySelector('.sidebar'),
+  addProject: document.querySelector('.sb-p-title'),
+  newProjectForm: document.querySelector('.new-project')
 }
 
 elements.menu.addEventListener('click', () => {
   elements.sidebar.classList.toggle('hide')
+});
+
+elements.addProject.addEventListener('click', () => {
+  const formStyle = elements.newProjectForm.style;
+  if (formStyle.display === 'none')
+    formStyle.display = 'flex';
+  else
+    formStyle.display = 'none';
 });
