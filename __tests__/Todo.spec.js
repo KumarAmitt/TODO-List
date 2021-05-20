e27e5772842d3590345b52c8a370d8fca0eeb9e7
KumarAmitt <amitt2015@outlook.com>
2021-05-20 23:19:07 +0530

    Add functions to read & write data in localStorage i Todo.spec.js

---
+++ __tests__/Todo.spec.js
@@ -0,0 +1,10 @@
+import Todo from '../src/js/Todo.js';
+
+const readData = () => {
+  const data = JSON.parse(localStorage.getItem('projects'));
+  return data || {};
+};
+
+const writeData = (data) => {
+  localStorage.setItem('projects', JSON.stringify(data));
+};
