5a92385120a3c1b4ca19416c0780198bc8d8d1bc
KumarAmitt <amitt2015@outlook.com>
2021-05-20 23:10:12 +0530

    Test to create a new Project Object

--- __tests__/Project.spec.js
+++ __tests__/Project.spec.js
@@ -8,3 +8,11 @@ const readData = () => {
 const writeData = (data) => {
   localStorage.setItem('projects', JSON.stringify(data));
 };
+
+describe('should create a Project object', () => {
+  const p = new Project('Project ONE');
+
+  test('should set name of Project', () => {
+    expect(p.name).toBe('Project ONE');
+  });
+});
