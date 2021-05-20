6383bf8b573df8d5802a09de21efd1763069f493
KumarAmitt <amitt2015@outlook.com>
2021-05-20 23:19:57 +0530

    Write dummy project data in localStorage

--- __tests__/Todo.spec.js
+++ __tests__/Todo.spec.js
@@ -8,3 +8,18 @@ const readData = () => {
 const writeData = (data) => {
   localStorage.setItem('projects', JSON.stringify(data));
 };
+
+const projects = {
+  'Project ONE': {
+    id01: {
+      title: 'Conference',
+      desc: 'Conference with Project Team',
+      ddt: '20-05-2021T15:00',
+      priority: 'high',
+      status: 'pending',
+    },
+  },
+  'Project TWO': {},
+};
+
+writeData(projects);
